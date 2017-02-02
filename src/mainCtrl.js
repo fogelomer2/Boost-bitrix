65c866907ea3bbde772cee41b77b1a46e827536e
ron ezra <ronezra3@gmail.com>
2017-02-03 01:41:19 +0200

    Backup firebase progress

--- src/mainCtrl.js
+++ src/mainCtrl.js
@@ -1,13 +1,34 @@
 app.controller("mainCtrl", function ($scope, $firebaseObject, $firebaseArray) {
 
-    var ref = firebase.database().ref().child("-KbtOdiIicnIsA4oGbMV");//.child("team").child("KbgME_4rFZvSQ8U6-mt");
-    var firebaseObject = $firebaseObject(ref);
+
+    var config = {
+        apiKey: "AIzaSyA-IwujsvtzgDx30Jws1br-Ivq7sbX9YyA",
+        authDomain: "boost-e7338.firebaseapp.com",
+        databaseURL: "https://boost-e7338.firebaseio.com",
+        storageBucket: "boost-e7338.appspot.com",
+        messagingSenderId: "712453368075"
+    };
+    firebase.initializeApp(config);
+
+
+    var database = firebase.database().ref();//.child("-KbtOdiIicnIsA4oGbMV");//.child("team").child("KbgME_4rFZvSQ8U6-mt");
+    //var database;
+    //Insert Obj - generate key
+    database.push({ "xx": { x: 1, y: 2 } }).then(function (res) {
+        res;
+    })
+    //Get elem by name
+    database.child("name").once("value").then(function (res) {
+        res;
+    });
+
+    //var firebaseObject = $firebaseObject(ref);
 
     $scope.questions = [
-        {text: 'Is', type: 1},
-        {text: 'What', type: 2, options: ['true','false','yes','no']},
-        {text: 'How', type: 3},
-        {text: 'How Much', type: 4},
+        { text: 'Is', type: 1 },
+        { text: 'What', type: 2, options: ['true', 'false', 'yes', 'no'] },
+        { text: 'How', type: 3 },
+        { text: 'How Much', type: 4 },
     ];
     $scope.questionsTypesEnum = {
         yesNo: 1,
@@ -17,18 +38,18 @@ app.controller("mainCtrl", function ($scope, $firebaseObject, $firebaseArray) {
     };
     $scope.answers = {}
 
-    firebaseObject.$loaded(
-        function (data) {
-            console.log(data["-KbtOdiIicnIsA4oGbMV"]);
-           // var players = data["-KbtOdiIicnIsA4oGbMV"].player;
-            firebaseObject
-            //$scope.questions = data[0].player.task[0].questions[0];
-            //$scope.$apply()
-        },
-        function (error) {
-            console.error("Error:", error);
-        }
-    );
+    // firebaseObject.$loaded(
+    //     function (data) {
+    //         console.log(data["-KbtOdiIicnIsA4oGbMV"]);
+    //         // var players = data["-KbtOdiIicnIsA4oGbMV"].player;
+    //         firebaseObject
+    //         //$scope.questions = data[0].player.task[0].questions[0];
+    //         //$scope.$apply()
+    //     },
+    //     function (error) {
+    //         console.error("Error:", error);
+    //     }
+    // );
 
     $scope.add = addNewItem;
     function addNewItem() {
